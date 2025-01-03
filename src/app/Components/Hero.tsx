6e75e9c247573bdbae011d6d3056e26dfb80cd59
ANZIE05 <161513339+ANZIE05@users.noreply.github.com>
2025-01-03 22:26:26 +0500

    done

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -16,7 +16,7 @@ const HeroPage = () => {
       <div className="relative z-10 flex flex-col justify-center text-justify h-full text-white px-8 md:px-16 lg:px-24">
         <h1 className="text-4xl md:text-5xl font-bold mb-10">The Journey Beyond <br/> Your Imaginary</h1>
         <p className="text-lg md:text-lg max-w-lg mb-6 text-white opacity-50">
-            Discover thousands of beautiful places around the world with wonderful experiences you can imagine. Experience the beauty of nature's creation.
+            "Discover thousands of beautiful places around the world with wonderful experiences you can imagine. Experience the beauty of nature&apos;s creation."
         </p>
         <button className="px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white font-semibold rounded-md shadow-lg w-auto self-start">
           <Link href="/Cards">Explore Now</Link>
